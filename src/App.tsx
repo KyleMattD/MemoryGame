85453009b0e77830c7093f3a78f625eb64f6d195
Kyle Drotsky <kyledrotsky@Kyles-MacBook-Air.local>
2021-11-12 20:59:38 +0200

    modal popup

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,29 @@ import './App.css';
 import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import Modal from '@mui/material/Modal';
 import Ballons from './assets/images/Group 30099.svg';
 import RocketShip from './assets/images/Group 30101.svg';
 
+
 export default function App(this: any) {
+  const [name, setName] = React.useState('');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [name1, setName1] = React.useState('');
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName1(event.target.value);
+  };
+  const [open1, setOpen1] = React.useState(false);
+  const handleOpen1 = () => setOpen1(true);
+  const handleClose1 = () => setOpen1(false);
+
   return (
     <div className="App">
         <div className="App-Header">
@@ -27,27 +41,43 @@ export default function App(this: any) {
         <div id="characters">
           <div id="p1">
             <img id="image" src={Ballons} alt='Balloons'/>
-            <Button onClick={handleOpen}>Open modal</Button>
             <Modal
               open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
+              id="modal"
             >
               <Box id="modal">
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Text in a modal
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Name of Player 1"
+                value={name}
+                onChange={handleChange}
+              />
+              <Button onClick={handleClose} color="success">Save</Button>
               </Box>
             </Modal>
-            <input id="space" placeholder="Name of Player 1"/>
+            <input id="space" placeholder="Name of Player 1" onClick={handleOpen} value={name}/>
           </div>
           <div id="p2">
             <img id="image" src={RocketShip} alt='RocketShip'/>
-            <input id="space" placeholder="Name of Player 2"/>
+            <Modal
+              open={open1}
+              id="modal"
+            >
+              <Box id="modal">
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Name of Player 2"
+                value={name1}
+                onChange={handleChange1}
+              />
+              <Button onClick={handleClose1} color="success">Save</Button>
+              </Box>
+            </Modal>
+            <input id="space" placeholder="Name of Player 2" onClick={handleOpen1} value={name1}/>
           </div>
         </div>
         <div>
@@ -59,3 +89,4 @@ export default function App(this: any) {
     </div>
   );
 }
+
